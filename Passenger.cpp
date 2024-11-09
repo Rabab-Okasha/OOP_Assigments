806ca351bfca712577f927abd854bff9c5e7ecd4
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-11-09 20:58:48 +0200

    Update Passenger.cpp
    Add void function in Passenger class and comments for the remain functions

--- Passenger.cpp
+++ Passenger.cpp
@@ -1,5 +1,17 @@
 #include "Passenger.h"
 #include <string>
 
+//initialize the static variable
 
+//2 args Constructor
 
+//Destructor
+
+//static function for the current total number of passengers
+
+//Display passenger's info
+void Passenger::Display() const{
+    cout << "Passenger's name: " << name << "\nPassenger's ID: " << ID << endl;
+}
+
+// insertion operator friend funcion >>
