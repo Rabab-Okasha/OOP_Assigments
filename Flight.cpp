ff954f33c7a7b612d6b75bf2b9f88e5ce998d34b
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-11-09 23:04:01 +0200

    Update Flight.cpp
    Add display function of class Flight with its comments

--- Flight.cpp
+++ Flight.cpp
@@ -6,3 +6,29 @@ using namespace std;
 int Flight::CurrentNoBookedOfSeats = 0;
 
 
+
+
+
+//Display flight details
+void Flight::Display() const{
+    //Displaying flight details
+   cout << "Flight Details: " << endl;
+   cout << "------------------------------";
+   cout << "\nNumber of flights: " << setw(8) << no_of_flights
+        << "\nSeating capacity: " << setw(8) << seating_capacity
+        << "\nDeparture Time: " << setw(11) << departure_time << " " << time_zone
+        << "\nDestination: " << setw(15) << flight_dest
+        << "\nSeating plan: ";
+   //Displaying the Seating plan
+   for(int i = 0; i < rows; i++){
+       for(int j = 0; j < columns; j++)
+           cout << setw(5) << seating_plan[i][j] << " ";
+       cout << endl;
+   }
+   cout << "\nPassengers' names: ";
+   //displaying passengers' names
+   for(int i = 0; i < seating_capacity; i++)
+       cout << "\nPassanger " << i + 1 << ": " << passengers_names[i];
+}
+
+
