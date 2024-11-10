ca4ae4e8b0a169d81be30434a87ad5c1cc088c0c
Basmala Nabil <154525200+Basmala-Nabil@users.noreply.github.com>
2024-11-10 22:13:15 +0200

    Update Passenger.cpp

--- Passenger.cpp
+++ Passenger.cpp
@@ -5,6 +5,19 @@
 
 //2 args Constructor
 
+Passenger::Passenger(string name , int id )
+{
+    passenger_name = name;
+    passenger_id = id;
+}
+istream &operator>>(istream &is ,  Passenger &p )
+{
+   cout<<"Enter your name : ";
+   is>>p.passenger_name;
+   cout<<"Enter your id : ";
+   is>>p.passenger_id;
+}
+
 //Destructor
 
 //static function for the current total number of passengers
