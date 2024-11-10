88d2bf974e6a0b1c14357bb522c418f2206c6165
Basmala Nabil <154525200+Basmala-Nabil@users.noreply.github.com>
2024-11-10 22:12:49 +0200

    Update Flight.cpp

--- Flight.cpp
+++ Flight.cpp
@@ -6,6 +6,23 @@ using namespace std;
 int Flight::CurrentNoBookedOfSeats = 0;
 
 
+Flight::Flight(string dest , int cap , int number)
+{
+   seating_capacity = cap;
+   no_of_flights = number;
+   flight_dest = dest;
+   seating_plan = new int*[seating_capacity/columns];
+   for(int i=0 ; i<seating_capacity/columns ; i++)
+   {
+       seating_plan[i]= new int[columns];
+
+       for(int j=0 ; j<columns;j++)
+
+           seating_plan[i][j]=0;
+   }
+   passengers_names = new string [seating_capacity];
+}
+
 
 
 
