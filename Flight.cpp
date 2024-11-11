566e0d56a8760e2ba5f8c626aafe1d932332b3b9
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-11-11 21:56:41 +0200

    Updating Display function & using rows variable

--- Flight.cpp
+++ Flight.cpp
@@ -5,26 +5,21 @@ using namespace std;
 //initialize the static variable
 int Flight::CurrentNoBookedOfSeats = 0;
 
-
+//3 args constructor
 Flight::Flight(string dest , int cap , int number)
 {
    seating_capacity = cap;
    no_of_flights = number;
    flight_dest = dest;
-   seating_plan = new int*[seating_capacity/columns];
-   for(int i=0 ; i<seating_capacity/columns ; i++)
+   seating_plan = new int*[rows];
+   for(int i=0 ; i < rows ; i++)
    {
        seating_plan[i]= new int[columns];
-
-       for(int j=0 ; j<columns;j++)
-
+       for(int j=0 ; j < columns;j++)
            seating_plan[i][j]=0;
    }
    passengers_names = new string [seating_capacity];
 }
-
-
-
 
 //Display flight details
 void Flight::Display() const{
@@ -35,17 +30,17 @@ void Flight::Display() const{
         << "\nSeating capacity: " << setw(8) << seating_capacity
         << "\nDeparture Time: " << setw(11) << departure_time << " " << time_zone
         << "\nDestination: " << setw(15) << flight_dest
-        << "\nSeating plan: ";
+        << "\nSeating plan: " << endl;
    //Displaying the Seating plan
    for(int i = 0; i < rows; i++){
        for(int j = 0; j < columns; j++)
-           cout << setw(5) << seating_plan[i][j] << " ";
+           cout << setw(5) << left << seating_plan[i][j] << " ";
        cout << endl;
    }
-   cout << "\nPassengers' names: ";
+   cout << "\nPassengers' names: " << endl;
    //displaying passengers' names
    for(int i = 0; i < seating_capacity; i++)
-       cout << "\nPassanger " << i + 1 << ": " << passengers_names[i];
+       cout << "Passanger " << i + 1 << ": " << passengers_names[i] << endl;
 }
 
 
