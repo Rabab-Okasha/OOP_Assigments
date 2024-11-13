7ad4023f7e990dea6d15ef66004bf269d3a1c604
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-11-14 00:09:53 +0200

    Provide test case for each function

--- main.cpp
+++ main.cpp
@@ -2,38 +2,63 @@
 #include <string>
 #include "Flight.h"
 #include "Passenger.h"
-
 using namespace std;
 
 int main(){
-
-    string des , n;
-   int capacity ,  num, passnum , id;
-    cout<<"Enter your destination : ";
-    cin>>des;
-    cout<<"Enter number of the flight : ";
-    cin>>num;
-    cout<<"Enter the capacity : ";
-    cin>>capacity;
-    Flight fl(des,capacity,num);
-
-    cout<<"add num of passengers : ";
-    cin>>passnum;
-        Passenger arr[passnum];
-    for(int i=0;i<passnum;i++)
-    {
-        cin>>arr[i];// cteate object for each index
-    }
-
-    fl.add_passengers(passnum, arr,  fl);
-    fl.Display();
-    Passenger p1;
-    cout<<"add passenger += \n";
-    cin>>p1;
-    fl+=p1;
-
-    fl.Display();in>>p1;
     
-    p1.Display_passenger();
+    int passengerNumbers;
+    Flight f1("London", 16, 123);
+    cout << "\nEnter number of passengers to be add: ";
+    cin >> passengerNumbers;
+    Passenger names [passnum];
+    for(int i = 0; i < names; i++)
+        cin >> names[i];// cteate object for each index
+    
+    //Testing function add_passenger
+    f1.add_passengers(passengerNumbers, arr,  f1);
+    f1.Display();
+    cout << f1 << endl;
+    
+    //Testing operator +=
+    Passenger p1;
+    cout << "\nAdd passenger(using +=): ";
+    cin >> p1;
+    f1 += p1;
+    //display flight details
+    f1.Display();
+    cout << f1 << endl;
+    //display passengers details
+    p1.Display();
+    cout << f1 << endl;
+    
+    //Testing Prefix operator ++
+    ++p1;
+    cout << p1 << endl;
+
+    //Testing copy constructor
+    cout << "\nUsing copy consturctor" << endl;
+    Flight f2 = fl;
+    f2.Display();
+    cout  << f2 << endl;
+    Passenger p;
+    cout << "Number of passengers in the system: ";
+    cout <<  p.get_noPassengers() << endl;
+
+    //Testing Postfix operator --
+    cout << "\nUsing Postfix --: " << endl;
+    cout << f1-- << endl;
+    Passenger p;
+    cout << "Number of passengers in the system(After removing 1): ";
+    cout <<  p.get_noPassengers() << endl;
+
+    //Testing operator -=
+    int removed_num;
+    cout << "\nEnter number of passengers to be removed: ";
+    cin >> removed_num;
+    f1 -= removed_num;
+    cout << f1 << endl;
+    cout << "\nNumber of passengers in the system" << "(After removing " << removed_num << " ):";
+    cout <<  p.get_noPassengers() << endl;
+
     return 0;
 }
