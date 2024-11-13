43d245c42a3bd82e5c3cc26b94a2361d1e450e85
Basmala Nabil <154525200+Basmala-Nabil@users.noreply.github.com>
2024-11-13 17:29:18 +0200

    classes  test 1

--- main.cpp
+++ main.cpp
@@ -7,5 +7,29 @@ using namespace std;
 
 int main(){
 
+    string des , n;
+   int capacity ,  num, passnum , id;
+    cout<<"Enter your destination : ";
+    cin>>des;
+    cout<<"Enter number of the flight : ";
+    cin>>num;
+    cout<<"Enter the capacity : ";
+    cin>>capacity;
+    Flight fl(des,capacity,num);
+
+    cout<<"add num of passengers : ";
+    cin>>passnum;
+        Passenger arr[passnum];
+    for(int i=0;i<passnum;i++)
+    {
+        cin>>arr[i];// cteate object for each index
+    }
+
+    fl.add_passengers(passnum, arr,  fl);
+    fl.Display();
+    Passenger p1;
+    cout<<"add passenger += \n";
+    cin>>p1;
+    p1.Display_passenger();
     return 0;
 }
