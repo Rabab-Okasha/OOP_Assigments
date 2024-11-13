2d609795f5facd1685484ef5cef1a1ae54e79992
Basmala Nabil <154525200+Basmala-Nabil@users.noreply.github.com>
2024-11-14 00:33:04 +0200

    removing unneeded variables , addig remove_passenger fn

--- Flight.h
+++ Flight.h
@@ -5,44 +5,47 @@
 using namespace std;
 
 class Flight {
-    friend ostream &operator<<(ostream &os, const Flight &flight);
-private:
-    
-    int no_of_flight;
+   friend ostream &operator<<(ostream &os, const Flight &flight);
+ private:
+    int no_of_flights;
     int seating_capacity;
     int departure_time;
     string time_zone;
     string flight_dest;
     int **seating_plan;
-    string** seating_names_plan; //??????
     string *passengers_names;
     const int columns = 4; // Assumption of number of seats Per row
     int rows = (seating_capacity / columns); //no of rows = capicity / no of columns
     int booked_seats = 0;
+   
+
+
  public:
      //3 args consturctor
      Flight(string dest , int cap , int number);
      //Operator prefix++
-     Flight &operator++();
-     //Operator +=
-     Flight operator+=(Passenger & p);
+     Flight &operator ++();
+      //Operator +=
+     Flight &operator +=(Passenger & p);
      //Operator postfix--
-     Flight operator--(int);
+     Flight operator --();
      //Operator -=
-     Flight operator-=(int num);
-     //Add passengers to a flight
-     void add_passengers(int passengers_number, Passenger names_of_passenger[], Flight& f);
+     Flight operator -=(int num);
      //Remove passenger methode
+     Flight &remove_passenger(Passenger &p2);
      //Search by name
      bool search_name(string nm);
      //Search by seat number
      void search_seatNo(int r, int c);
+     //Add passengers to a flight
+     void add_passengers(int passengers_number, Passenger names_of_passenger[], Flight& f);
      //Display flight details
      void Display() const;
-    //Copy constructor
+     //Copy constructor
      Flight(const Flight &obj);
      //Destructor
-     ~Flight();   
+     ~Flight();
+
 };
 
 
