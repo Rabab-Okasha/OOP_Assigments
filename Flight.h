12741429a2ea5cff14c14ae9f87876bd309a1d52
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-11-23 20:04:46 +0200

    Add comments to the last updated version

--- Flight.h
+++ Flight.h
@@ -5,45 +5,44 @@
 using namespace std;
 
 class Flight {
-friend ostream &operator<<(ostream &os, const Flight &flight);
- private:
-    int no_of_flights;//number of the flight
-    int seating_capacity;// capacity
+   friend ostream &operator<<(ostream &os, const Flight &flight);
+private:
+    int no_of_flight;
+    int seating_capacity;
     string departure_time;
     string time_zone;
     string flight_dest;
-    int **seating_plan;// array of seats
-    string *passengers_names;// array of passenger names
+    int **seating_plan;
+    string *passengers_names;
     const int columns = 4; // Assumption of number of seats Per row
-    int rows = (seating_capacity / columns); //no of rows = capacity / no of columns
-    int booked_seats = 0;
+    int rows = (seating_capacity / columns); //no. of rows = capicity / no. of columns
+    int booked_seats = 0; //number of booked seats in the flight
 
-
- public:
-     //3 args consturctor
-     Flight(string dest , int cap , int number);
-     //Operator prefix++
-     Flight &operator ++();
-      //Operator +=
-     Flight &operator +=(Passenger & p);
-     //Operator postfix--
-     Flight operator --(int);
-     //Operator -=
-     Flight &operator -=(int num);
-     //Remove passenger methode
-     Flight &remove_passenger(Passenger &p2);
-     //Search by name
-     bool search_name(string nm);
-     //Search by seat number
-     void search_seatNo(int r, int c);
-     //Add passengers to a flight
-     void add_passengers(int passengers_number, Passenger names_of_passenger[]);
-     //Display flight details
-     void Display() const;
-     //Copy constructor
-     Flight(const Flight &obj);
-     //Destructor
-     ~Flight();
+public:
+    //3 args consturctor
+    Flight(string dest , int cap , int number);
+    //Operator prefix++
+    Flight &operator++();
+    //Operator +=
+    Flight &operator+=(Passenger &p);
+    //Operator postfix--
+    Flight operator--(int);
+    //Operator -=
+    Flight &operator-=(int num);
+    //Add passengers to a flight
+    void add_passengers(int number_of_passengers, Passenger names_of_passengers[]);
+    //Remove passenger methode
+    Flight &remove_passenger(Passenger &p2);
+    //Search by name
+    bool search_name(string nm);
+    //Search by seat number
+    void search_seatNo(int r, int c);
+    //Display flight details
+    void Display() const;
+    //Copy constructor
+    Flight(const Flight &obj);
+    //Destructor
+    ~Flight();
 };
 
 
