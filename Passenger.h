28f68e9fe3a4b30eed2045e20e33be736c558a53
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-11-23 20:03:36 +0200

    Add last version of the code

--- Passenger.h
+++ Passenger.h
@@ -4,29 +4,27 @@
 using namespace std;
 
 class Passenger {
-// add class Flight as a friend class to access member functions in Flight class
+    //make class Flight friend to class Passenger to access private data of class passenger
     friend class Flight;
     // overloading operator >>
-    friend istream& operator>>(istream &is ,  Passenger &p );
-    private:
-        string passenger_name;
-        int passenger_id;
-    public:
-        static int CountTotalPassengers;
-        // default constructor
-        Passenger();
-        // 2 args constructor
-        Passenger(string name , int id);
-        // get passenger name fn
-        string getname()const;
-        //static function for the current total number of passengers
-        static int get_noPassenger();
-        //Display passenger info
-        void Display()const;
-        //Destructor
-        ~Passenger();
-
-  
+    friend istream& operator>>(istream &is,  Passenger &p);
+private:
+    string passenger_name;
+    int passenger_id;
+    static int CountTotalPassengers; // number of all passengers in system
+public:
+    // default constructor
+    Passenger();
+    // 2 args constructor
+    Passenger(string name, int id);
+    //static function for the current total number of passengers
+    static int get_No_Passengers();
+    // get passenger name fn
+    string getname()const;
+    //Destructor
+    ~Passenger();
+    //Display passenger info
+    void Display() const;
 };
 
 
