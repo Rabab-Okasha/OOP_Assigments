8083375e05af4a2026808f1851b73fae0f478787
Rabab-Okasha <rababokasha123@gmail.com>
2024-12-19 10:43:44 +0200

    Add Assigments files

---
+++ Assigment_1/Flight.cpp
@@ -0,0 +1,8 @@
+#include "Flight.h"
+#include <string>
+using namespace std;
+
+//initialize the static variable
+int Flight::CurrentNoBookedOfSeats = 0;
+
+
---
+++ Assigment_1/Passenger.h
@@ -0,0 +1,15 @@
+#ifndef OOPPROJECTS_PASSENGER_H
+#define OOPPROJECTS_PASSENGER_H
+#include <string>
+using namespace std;
+
+class Passenger {
+private:
+
+public:
+
+};
+
+
+
+#endif //OOPPROJECTS_PASSENGER_H
