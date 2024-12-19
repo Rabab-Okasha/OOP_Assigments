194f2ba9f51b8e4112d05268b5d8721a9fa88071
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-12-19 15:48:21 +0200

    Update SurgerySchedule.cpp

--- Hospital-system/SurgerySchedule.cpp
+++ Hospital-system/SurgerySchedule.cpp
@@ -1,1 +1,30 @@
 #include "SurgerySchedule.h"
+
+//default constructor
+SurgerySchedule::SurgerySchedule()
+     :patientName(""), Date(""){}
+
+//parametized constructor
+SurgerySchedule::SurgerySchedule(string &PN, string &date)
+    :patientName(PN), Date(date){
+}
+
+//seter for patient name
+void SurgerySchedule::set_patientName(string &pn){
+    patientName = pn;
+}
+
+//seter for date
+void SurgerySchedule::set_date(string &date){
+    Date = date;
+}
+
+//getter for patient name
+string SurgerySchedule::get_patientName()const{
+    return patientName;
+}
+
+//getter for date
+string SurgerySchedule::get_date()const{
+    return Date;
+}
