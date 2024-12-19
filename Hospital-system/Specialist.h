a43927b4a820c8c80d8146fda04a1d489c81241b
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-12-19 15:44:18 +0200

    Update Specialist.h

--- Hospital-system/Specialist.h
+++ Hospital-system/Specialist.h
@@ -1,9 +1,20 @@
 #ifndef OOPPROJECTS_SPECIALIST_H
 #define OOPPROJECTS_SPECIALIST_H
 #include "Doctor.h"
+#include "Surgeon.h"
+#include <string>
 
-class Specialist: public Doctor {
-
+class Specialist: public Doctor{
+    string speciality;
+public:
+    //default constructor
+    Specialist();
+    //parameterized constructor
+    Specialist(string &id, string &nm, int dur, float r,string &sp);
+    //overriden calculatetotalfee function
+    double calculateTotalFee() override;
+    //overriden prettyprint fucntion
+    void PrettyPrint() override;
 };
 
 
