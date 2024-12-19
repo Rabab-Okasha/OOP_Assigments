643dfc49c4ef9cb88f71cb30d076d6af4ff66fc0
Rabab-Okasha <rababokasha123@gmail.com>
2024-12-19 15:52:50 +0200

    Delete duplicated files

--- Assigment_1/Flight.h
+++ Assigment_1/Flight.h
@@ -1,25 +1,51 @@
+//Rabab Mohamed Abd El-Gafour 20237004  S2
+//Basmala Nabil El-sayed      20235005  S11
+
 #ifndef OOPPROJECTS_FLIGHT_H
 #define OOPPROJECTS_FLIGHT_H
 #include <string>
+#include "Passenger.h"
 using namespace std;
 
 class Flight {
-    friend class Passenger;
     friend ostream &operator<<(ostream &os, const Flight &flight);
 private:
-    int no_of_flights;
+    int no_of_flight;
     int seating_capacity;
-    int departure_time;
+    string departure_time;
     string time_zone;
     string flight_dest;
     int **seating_plan;
     string *passengers_names;
-    static int CurrentNoBookedOfSeats;
-    const int no_of_rows = 4;
+    const int columns = 4; // Assumption of number of seats Per row
+    int rows = (seating_capacity / columns); //no. of rows = capicity / no. of columns
+    int booked_seats = 0; //number of booked seats in the flight
+
 public:
-
+    //3 args consturctor
+    Flight(string dest , int cap , int number);
+    //Operator prefix++
+    Flight &operator++();
+    //Operator +=
+    Flight &operator+=(Passenger &p);
+    //Operator postfix--
+    Flight operator--(int);
+    //Operator -=
+    Flight &operator-=(int num);
+    //Add passengers to a flight
+    void add_passengers(int number_of_passengers, Passenger names_of_passengers[]);
+    //Remove passenger methode
+    Flight &remove_passenger(Passenger &p2);
+    //Search by name
+    bool search_name(string nm);
+    //Search by seat number
+    void search_seatNo(int r, int c);
+    //Display flight details
+    void Display() const;
+    //Copy constructor
+    Flight(const Flight &obj);
+    //Destructor
+    ~Flight();
 };
-
-
 
 #endif //OOPPROJECTS_FLIGHT_H
--- Assigment_1/Passenger.cpp
+++ Assigment_1/Passenger.cpp
@@ -1,5 +1,54 @@
+//Rabab Mohamed Abd El-Gafour 20237004  S2
+//Basmala Nabil El-sayed      20235005  S11
+
 #include "Passenger.h"
+#include <iostream>
 #include <string>
 
+// Initialize the static variable
+int Passenger::CountTotalPassengers = 0;
 
+// Default constructor
+Passenger::Passenger() {}
 
+// Constructor with two arguments
+Passenger::Passenger(string name, int id){
+    // initializing private variables
+    passenger_name = name;
+    passenger_id = id;
+}
+
+// Static function to get current total number of passengers
+int Passenger::get_No_Passengers() {
+    return CountTotalPassengers;
+}
+
+// Get passenger name
+string Passenger::getname() const {
+    return passenger_name;
+}
+
+// Insertion operator overload
+istream &operator>>(istream &is, Passenger &p) {
+    // taking passenger name and initializing it to private variable
+    cout << "Enter Passenger name : ";
+    is >> p.passenger_name;
+    // taking passenger ID and initializing it to private variable
+    cout << "Enter Passenger ID : ";
+    is >> p.passenger_id;
+    return is;
+}
+
+// Destructor
+Passenger::~Passenger() {
+    //for each destroyed passenger object decrement the total number of passengers
+    CountTotalPassengers--;
+    //massage that indicates the object has been destroyed
+    cout << "Destructor of Passenger class has been called" << endl;
+}
+
+// Display passenger's info
+void Passenger::Display() const {
+    cout << "\nPassenger's name: " << passenger_name
+         << "\nPassenger's ID: " << passenger_id << endl;
+}
