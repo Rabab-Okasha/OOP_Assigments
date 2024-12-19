eb2e9b8793c0aab884ea1ae8a3b9e0bb5ccfee76
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-12-19 15:43:07 +0200

    Add doctor.h code

--- Hospital-system/Doctor.h
+++ Hospital-system/Doctor.h
@@ -1,12 +1,23 @@
 #ifndef OOPPROJECTS_DOCTOR_H
 #define OOPPROJECTS_DOCTOR_H
 #include <string>
+using namespace std;
 
 class Doctor {
 protected:
-string id;
-
-
+    string ID;
+    string name;
+    int duration;
+    float rate;
+public:
+    //default constructor
+    Doctor();
+    //paramterized constructor
+    Doctor(string &id, string &nm, int dur, float r);
+    //function prettyprint
+    virtual void PrettyPrint();
+    //function calculatetotlafee
+    virtual double calculateTotalFee();
 };
 
 
