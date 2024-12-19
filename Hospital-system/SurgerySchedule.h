c31e42a5d227b29ab58b4d9570c41c11addece39
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-12-19 15:45:59 +0200

    Update SurgerySchedule.h

--- Hospital-system/SurgerySchedule.h
+++ Hospital-system/SurgerySchedule.h
@@ -1,8 +1,25 @@
 #ifndef OOPPROJECTS_SURGERYSCHEDULE_H
 #define OOPPROJECTS_SURGERYSCHEDULE_H
+#include <string>
+using namespace std;
 
-
-class SurgerySchedule {
+class SurgerySchedule{
+private:
+    string patientName;
+    string Date;
+public:
+    //default constructor
+    SurgerySchedule();
+    //parametized constructor
+    SurgerySchedule(string &PN, string &date);
+    //seter for patient name
+    void set_patientName(string &pn);
+    //seter for date
+    void set_date(string &date);
+    //getter for patient name
+    string get_patientName()const;
+    //getter for date
+    string get_date()const;
 
 };
 
