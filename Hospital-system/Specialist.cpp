c7ac6f1f3a7c4f0566fea58cbc90c85fe466d659
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-12-19 15:44:37 +0200

    Update Specialist.cpp

--- Hospital-system/Specialist.cpp
+++ Hospital-system/Specialist.cpp
@@ -1,1 +1,19 @@
 #include "Specialist.h"
+#include <iostream>
+
+//default constructor
+Specialist::Specialist()
+    :Doctor(), speciality("") {}
+
+//parameterized constructor
+Specialist::Specialist(string &id, string &nm, int dur, float r,string &sp)
+    :Doctor(id, nm, dur, r), speciality(sp){}
+
+//overriden calculatetotalfee function
+double Specialist::calculateTotalFee(){
+    return (duration/15.0) * rate;
+}
+//overriden prettyprint fucntion
+void Specialist::PrettyPrint(){
+    cout << "Dr. " << name << " is a Specialist with whose total  fee is " << calculateTotalFee() << endl;
+}
