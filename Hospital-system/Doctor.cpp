5afa8418fa9f6b1400452bf003196c6386be8622
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-12-19 15:43:42 +0200

    Update Doctor.cpp

--- Hospital-system/Doctor.cpp
+++ Hospital-system/Doctor.cpp
@@ -1,1 +1,20 @@
 #include "Doctor.h"
+#include <iostream>
+
+//default constructor
+Doctor::Doctor()
+   :ID(""), name(""), duration(0), rate(0.0){}
+
+//paramterized constructor
+Doctor::Doctor(string &id, string &nm, int dur, float r)
+    :ID(id), name(nm), duration(dur), rate(r){}
+
+//function prettyprint
+void Doctor::PrettyPrint(){
+    cout << "Doctor ID is " << ID << ", and name is Dr. " << name << endl;
+}
+
+//function calculatetotlafee
+double Doctor::calculateTotalFee(){
+    return 0.0;
+}
