27f32731b7c93ee4eb27866fc6682b676b9fba64
Rabab Okasha <154630281+Rabab-Okasha@users.noreply.github.com>
2024-12-23 19:46:22 +0200

    Add the f

--- Hospital-system/Doctor.h
+++ Hospital-system/Doctor.h
@@ -1,1 +1,29 @@
+#ifndef OOPPROJECTS_DOCTOR_H
+#define OOPPROJECTS_DOCTOR_H
 
+#include <string>
+
+using namespace std;
+
+class Doctor {
+protected:
+    string ID;
+    string name;
+    int duration; //total number of minutes Dr spent with patients
+    float rate;
+public:
+    //default constructor
+    Doctor();
+
+    //paramterized constructor that set doctor's Id, name, duration and rate
+    Doctor(string &id, string &Name, int Duration, float Rate);
+
+    //print doctor details (Id and name)
+    virtual void PrettyPrint();
+
+    //Pure virtual function that Calculate total fees of doctor's service (it's implemented in class Surgeon & Splecialist)
+    virtual double calculateTotalFee() = 0;
+};
+
+
+#endif 
